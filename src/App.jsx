d0f7fde9e53899ed0815e5a86e9246c0bcddacf7
Emma <em.smyth5@gmail.com>
2024-01-06 22:54:03 +0000

    add error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import Home from "./UI/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Home from "./UI/Home";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
 import AppLayout from "./UI/AppLayout";
+import Error from "./UI/Error";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       { path: "/", element: <Home /> },
       {
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
