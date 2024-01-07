55396ec2e951ba5c9c90864b719c3fa04701a166
Emma <em.smyth5@gmail.com>
2024-01-07 22:19:43 +0000

    set up new order form

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Home from "./UI/Home";
 import Cart from "./features/cart/Cart";
-import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./UI/AppLayout";
 import Error from "./UI/Error";
 
@@ -24,8 +26,17 @@ const router = createBrowserRouter([
         path: "/cart",
         element: <Cart />,
       },
-      { path: "/order/new", element: <CreateOrder /> },
-      { path: "/order/orderId", element: <Order /> },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
+      {
+        path: "/order/orderId",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
     ],
   },
 ]);
